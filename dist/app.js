edfb7dcec8102d0b03ed2ca85ecc9c1796efa2db
Nikoloz Imerlishvili <65135792+callmenikk@users.noreply.github.com>
2021-08-03 21:05:43 +0400

    added features

--- dist/app.js
+++ dist/app.js
@@ -16,14 +16,14 @@ const discord_js_1 = require("discord.js");
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const dotenv_1 = require("dotenv");
 dotenv_1.config({
-    path: __dirname + "/.env"
+    path: __dirname + "/.env",
 });
 const client = new discord_js_1.Client();
 client.on("ready", () => {
     console.log("I'm ready");
 });
 client.on("message", (message) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b, _c, _d;
+    var _a, _b, _c, _d, _e;
     const prefix = "!";
     if (message.author.bot)
         return;
@@ -31,20 +31,23 @@ client.on("message", (message) => __awaiter(void 0, void 0, void 0, function* ()
         return;
     if (!message.content.startsWith(prefix))
         return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(/ +/g);
     const cmd = (_a = args.shift()) === null || _a === void 0 ? void 0 : _a.toLowerCase();
     if (cmd === "ping") {
         message.channel.send(`🏓 Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
     if (cmd === "quote") {
         node_fetch_1.default("https://type.fit/api/quotes")
-            .then(response => response.json())
-            .then(quote => {
+            .then((response) => response.json())
+            .then((quote) => {
             const random = Math.floor(Math.random() * quote.length);
             const { text, author } = quote[random];
             const msg = message.channel.send(`*${text}* - **${author}**`);
         })
-            .catch(err => console.log(err));
+            .catch((err) => console.log(err));
     }
     if (cmd === "command") {
         message.channel.send(" ```I only have\n!ping\n!quote\nI'm under Development Process, some new Features will be added soon ```  ");
@@ -53,11 +56,13 @@ client.on("message", (message) => __awaiter(void 0, void 0, void 0, function* ()
         if (message.deletable)
             message.delete();
         if (args.length < 1)
-            message.reply("nothing to say")
-                .then(m => m.delete({ timeout: 5000, reason: "idk" }));
+            message
+                .reply("nothing to say")
+                .then((m) => m.delete({ timeout: 5000, reason: "idk" }));
         if (args[0].toLowerCase() === "embed") {
+            const userHEX = (_b = message.member) === null || _b === void 0 ? void 0 : _b.displayHexColor;
             const embed = new discord_js_1.MessageEmbed()
-                .setColor("#dd0ef0")
+                .setColor(userHEX)
                 .setDescription(args.slice(1).join(" "))
                 .setTimestamp();
             message.channel.send(embed);
@@ -71,16 +76,19 @@ client.on("message", (message) => __awaiter(void 0, void 0, void 0, function* ()
     }
     if (cmd === "whois") {
         const userProfile = message.author.avatarURL();
-        const userRoles = (_b = message.member) === null || _b === void 0 ? void 0 : _b.roles.cache.map(r => r.name).slice(0, -1).toString().replace(/ ,/g, ",");
-        const userHEX = (_c = message.member) === null || _c === void 0 ? void 0 : _c.displayHexColor;
-        const createArray = message.author.createdAt.toString().split(" ");
+        const userRolesLength = (_c = message.member) === null || _c === void 0 ? void 0 : _c.roles.cache.map((r) => r.name).slice(0, -1);
+        const userHEX = (_d = message.member) === null || _d === void 0 ? void 0 : _d.displayHexColor;
+        const Roles = userRolesLength.map((e) => e);
+        const createArray = message.author.createdAt
+            .toString()
+            .split(" ");
         const createDate = `${createArray[1]} ${createArray[2]} - ${createArray[3]}`;
-        const joinedArray = String((_d = message.guild.me) === null || _d === void 0 ? void 0 : _d.joinedAt).split(" ");
+        const joinedArray = String((_e = message.guild.me) === null || _e === void 0 ? void 0 : _e.joinedAt).split(" ");
         const joinedDate = `${joinedArray[1]} ${joinedArray[2]} - ${joinedArray[3]}`;
         const exampleEmbed = new discord_js_1.MessageEmbed()
             .setColor(userHEX)
             .setTitle("User Info")
-            .setAuthor(`@${message.author.username}`, userProfile)
+            .setAuthor(`${message.author.tag}`, userProfile)
             .setThumbnail(userProfile)
             .setDescription("Some description here")
             .addFields({
@@ -92,15 +100,62 @@ client.on("message", (message) => __awaiter(void 0, void 0, void 0, function* ()
             value: createDate,
             inline: true,
         }, {
-            name: `Roles [${userRoles === null || userRoles === void 0 ? void 0 : userRoles.split(" ").length}]`,
-            value: userRoles,
-            inline: true,
+            name: `Roles [${userRolesLength === null || userRolesLength === void 0 ? void 0 : userRolesLength.length}]`,
+            value: Roles,
+            inline: false,
         })
             .setTimestamp()
             .setFooter(message.author.id, userProfile);
         message.channel.send(exampleEmbed);
     }
+    if (cmd === "roll") {
+        const dice = Math.floor(Math.random() * 5) + 1;
+        message.channel.send(`You get the ${dice}`);
+    }
+    if (cmd === "covid") {
+        node_fetch_1.default("https://api.covid19api.com/summary")
+            .then((response) => response.json())
+            .then((stats) => {
+            const CovidData = stats.Global;
+            const embed = new discord_js_1.MessageEmbed()
+                .setColor("#4f1e1b")
+                .setTitle("Covid-19 Global Stats")
+                .setThumbnail("https://images.newscientist.com/wp-content/uploads/2020/02/11165812/c0481846-wuhan_novel_coronavirus_illustration-spl.jpg")
+                .addFields({
+                name: "New Confirmed",
+                value: String(CovidData.NewConfirmed).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+                inline: true,
+            }, {
+                name: "Total Confirmed",
+                value: String(CovidData.TotalConfirmed).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+                inline: true,
+            }, {
+                name: "New Deaths",
+                value: String(CovidData.NewDeaths).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+                inline: true,
+            }, {
+                name: "New Recovered",
+                value: String(CovidData.NewRecovered).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+                inline: false,
+            }, {
+                name: "Total Recovered",
+                value: String(CovidData.TotalRecovered).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+                inline: false,
+            });
+            message.channel.send(embed);
+        })
+            .catch((err) => console.log(err));
+    }
 }));
-client.on("guildMemberAdd", (newMember) => __awaiter(void 0, void 0, void 0, function* () {
+client.on("message", (message) => __awaiter(void 0, void 0, void 0, function* () {
+    if (message.author.bot)
+        return;
+    if (!message.guild)
+        return;
+    const raw = message.content;
+    node_fetch_1.default(`https://api.promptapi.com/bad_words?censor_character=${raw}`)
+        .then((response) => response.json())
+        .then((badword) => console.log(badword))
+        .catch((err) => console.log(err));
 }));
 client.login(process.env.TOKEN);
--- src/app.ts
+++ src/app.ts
@@ -1,120 +1,209 @@
-import {Client, MessageEmbed} from "discord.js"
-import fetch from "node-fetch"
-import {config} from "dotenv"
+import { Client, MessageEmbed } from "discord.js";
+import fetch from "node-fetch";
+import { config } from "dotenv";
+
+
 
 config({
-    path: __dirname + "/.env"
-})
+  path: __dirname + "/.env",
+});
 
-type UserMessage = string | undefined
-type NONE = null | undefined
+type UserMessage = string | undefined;
 
-interface QuoteInterface<T>{
-    text: string,
-    author: T
+interface QuoteInterface<T> {
+  text: string;
+  author: T;
 }
 
-const client = new Client()
+const client = new Client();
 
-client.on("ready", ()=> {
-    console.log("I'm ready")
-})
+client.on("ready", () => {
+  console.log("I'm ready");
+});
 
-client.on("message", async message => {
-    const prefix: string = "!"
+client.on("message", async (message) => {
+  const prefix: string = "!";
 
-    if(message.author.bot) return
-    if(!message.guild) return
-    if(!message.content.startsWith(prefix)) return
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    const args: string[] = message.content.slice(prefix.length).trim().split(/ +/g)
-    const cmd: UserMessage = args.shift()?.toLowerCase() 
+  const args: string[] = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd: UserMessage = args.shift()?.toLowerCase();
 
-    if(cmd === "ping") {
-         message.channel.send(`🏓 Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  if (cmd === "ping") {
+    message.channel.send(
+      `🏓 Latency is ${
+        Date.now() - message.createdTimestamp
+      }ms. API Latency is ${Math.round(client.ws.ping)}ms`
+    );
+  }
+  if (cmd === "quote") {
+    fetch("https://type.fit/api/quotes")
+      .then((response) => response.json())
+      .then((quote) => {
+        const random: number = Math.floor(Math.random() * quote.length);
+        const { text, author }: QuoteInterface<string> = quote[random];
+
+        const msg = message.channel.send(`*${text}* - **${author}**`);
+      })
+      .catch((err) => console.log(err));
+  }
+  if (cmd === "command") {
+    message.channel.send(
+      " ```I only have\n!ping\n!quote\nI'm under Development Process, some new Features will be added soon ```  "
+    );
+  }
+  if (cmd === "say") {
+    if (message.deletable) message.delete();
+
+    if (args.length < 1)
+      message
+        .reply("nothing to say")
+        .then((m) => m.delete({ timeout: 5000, reason: "idk" }));
+
+    if (args[0].toLowerCase() === "embed") {
+      const userHEX: any = message.member?.displayHexColor;
+
+      const embed = new MessageEmbed()
+        .setColor(userHEX)
+        .setDescription(args.slice(1).join(" "))
+        .setTimestamp();
+      message.channel.send(embed);
     }
-    if(cmd === "quote"){
-         fetch("https://type.fit/api/quotes")
-        .then(response => response.json())
-        .then(quote => {
-            const random: number = Math.floor(Math.random() * quote.length)
-            const {text, author}: QuoteInterface<string> = quote[random]
+  }
+  if (cmd === "obamaballs") {
+    message.channel.send(
+      "https://media.tenor.com/images/c6755016355961ff8f9a4301d4bbb07d/tenor.png"
+    );
+  }
+  if (cmd === "sussybaka") {
+    message.channel.send(
+      "https://cdn.discordapp.com/attachments/871824935016865858/871846793879638087/static-assets-upload2210855008168198565.jpg"
+    );
+  }
+  if (cmd === "whois") {
+    const userProfile: any = message.author.avatarURL();
+    const userRolesLength: any = message.member?.roles.cache
+      .map((r) => r.name)
+      .slice(0, -1);
+    const userHEX: any = message.member?.displayHexColor;
+    const Roles: string = userRolesLength.map((e: any) => e);
 
-            const msg = message.channel.send(`*${text}* - **${author}**`)
-        })
-        .catch(err => console.log(err))
-    }
-    if(cmd === "command"){
-        message.channel.send(" ```I only have\n!ping\n!quote\nI'm under Development Process, some new Features will be added soon ```  ")
-    }
-    if(cmd === "say"){
-        if(message.deletable) message.delete()
+    const createArray: string[] = message.author.createdAt
+      .toString()
+      .split(" ");
+    const createDate: string = `${createArray[1]} ${createArray[2]} - ${createArray[3]}`;
 
-        if(args.length < 1) message.reply("nothing to say")
-        .then(m => m.delete({timeout: 5000, reason: "idk"}))
+    const joinedArray: string[] = String(message.guild.me?.joinedAt).split(" ");
+    const joinedDate: string = `${joinedArray[1]} ${joinedArray[2]} - ${joinedArray[3]}`;
 
-        if(args[0].toLowerCase() === "embed"){
-            const embed = new MessageEmbed()
-              .setColor("#dd0ef0")
-              .setDescription(args.slice(1).join(" "))
-              .setTimestamp()
-            message.channel.send(embed)
+    const exampleEmbed = new MessageEmbed()
+      .setColor(userHEX)
+      .setTitle("User Info")
+      .setAuthor(`${message.author.tag}`, userProfile)
+      .setThumbnail(userProfile)
+      .setDescription("Some description here")
+      .addFields(
+        {
+          name: "Joined",
+          value: joinedDate,
+          inline: true,
+        },
+        {
+          name: "Registered",
+          value: createDate,
+          inline: true,
+        },
+        {
+          name: `Roles [${userRolesLength?.length}]`,
+          value: Roles,
+          inline: false,
         }
-    }
-    if(cmd === "obamaballs"){
-        message.channel.send(
-          "https://media.tenor.com/images/c6755016355961ff8f9a4301d4bbb07d/tenor.png"
-        );
-    }
-    if(cmd === "sussybaka"){
-        message.channel.send(
-          "https://cdn.discordapp.com/attachments/871824935016865858/871846793879638087/static-assets-upload2210855008168198565.jpg"
-        );
-    }
-    if(cmd === "whois"){
-        const userProfile: any  = message.author.avatarURL()
-        const userRoles: UserMessage = message.member?.roles.cache.map(r => r.name).slice(0, -1).toString().replace(/ ,/g, ",")
-        const userHEX: any = message.member?.displayHexColor;
+      )
+      .setTimestamp()
+      .setFooter(message.author.id, userProfile);
 
-        const createArray: string[] = message.author.createdAt.toString().split(" ")
-        const createDate: string = `${createArray[1]} ${createArray[2]} - ${createArray[3]}`
+    message.channel.send(exampleEmbed);
+  }
+  if (cmd === "roll") {
+    const dice: number = Math.floor(Math.random() * 5) + 1;
 
-        const joinedArray: string[] = String(message.guild.me?.joinedAt).split(" ");
-        const joinedDate: string = `${joinedArray[1]} ${joinedArray[2]} - ${joinedArray[3]}`
+    message.channel.send(`You get the ${dice}`);
+  }
+  if (cmd === "covid") {
+    fetch("https://api.covid19api.com/summary")
+      .then((response) => response.json())
+      .then((stats) => {
+        const CovidData: any = stats.Global;
 
-        const exampleEmbed = new MessageEmbed()
-          .setColor(userHEX)
-          .setTitle("User Info")
-          .setAuthor(`@${message.author.username}`, userProfile)
-          .setThumbnail(userProfile)
-          .setDescription("Some description here")
+        const embed = new MessageEmbed()
+          .setColor("#4f1e1b")
+          .setTitle("Covid-19 Global Stats")
+          .setThumbnail(
+            "https://images.newscientist.com/wp-content/uploads/2020/02/11165812/c0481846-wuhan_novel_coronavirus_illustration-spl.jpg"
+          )
           .addFields(
             {
-              name: "Joined",
-              value: joinedDate,
+              name: "New Confirmed",
+              value: String(CovidData.NewConfirmed).replace(
+                /\B(?=(\d{3})+(?!\d))/g,
+                ","
+              ),
               inline: true,
             },
             {
-              name: "Registered",
-              value: createDate,
+              name: "Total Confirmed",
+              value: String(CovidData.TotalConfirmed).replace(
+                /\B(?=(\d{3})+(?!\d))/g,
+                ","
+              ),
               inline: true,
             },
             {
-              name: `Roles [${userRoles?.split(" ").length}]`,
-              value: userRoles,
+              name: "New Deaths",
+              value: String(CovidData.NewDeaths).replace(
+                /\B(?=(\d{3})+(?!\d))/g,
+                ","
+              ),
               inline: true,
+            },
+            {
+              name: "New Recovered",
+              value: String(CovidData.NewRecovered).replace(
+                /\B(?=(\d{3})+(?!\d))/g,
+                ","
+              ),
+              inline: false,
+            },
+            {
+              name: "Total Recovered",
+              value: String(CovidData.TotalRecovered).replace(
+                /\B(?=(\d{3})+(?!\d))/g,
+                ","
+              ),
+              inline: false,
             }
-          )
-          .setTimestamp()
-          .setFooter(message.author.id, userProfile);
+          );
+        message.channel.send(embed);
+      })
+      .catch((err) => console.log(err));
+  }
+});
 
-        message.channel.send(exampleEmbed)
-    }
-})
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (!message.guild) return;
 
+  const raw = message.content
 
-client.on("guildMemberAdd", async newMember => {
-    
-})
+  fetch(`https://api.promptapi.com/bad_words?censor_character=${raw}`)
+    .then((response) => response.json())
+    .then((badword) => console.log(badword))
+    .catch((err) => console.log(err));
+});
 
 client.login(process.env.TOKEN);
